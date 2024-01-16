a939ff8a00718c7c8aa5bbc493ea9d9922a3af94
viadel13 <shababizetre@gmail.com>
2024-01-16 10:13:44 +0100

    first commit

--- src/components/Apropos/Index.js
+++ src/components/Apropos/Index.js
@@ -1,194 +1,163 @@
 import React, { useEffect, useState } from 'react';
 import { Avatar, Backdrop, Box, CircularProgress, Container, Grid, IconButton, List, ListItem, ListItemAvatar, ListItemIcon, ListItemText, Paper, Stack, Typography } from '@mui/material';
-import heroIm from '../../assets/images/about.jpg';
+import heroIm from '../../assets/images/about.webp';
 import about2 from '../../assets/images/about2.jpg';
 import { DoubleArrow, PlayCircle, PlayCircleTwoTone } from '@mui/icons-material';
 
 const Apropos = () => {
 
-  const [chargement, setChargement] = useState(true);
-  const [load, setLoad] = useState(true);
-
-  useEffect(() => {
-    const image1 = new Image();
-    const image2 = new Image();
-
-    image1.src = heroIm;
-    image2.src = about2;
-
-    let imagesChargées = 0;
-
-    const gestionnaireChargementImage = () => {
-      imagesChargées++;
-
-      if (imagesChargées === 2) {
-        // Les deux images sont chargées, désactive le préchargeur
-        setChargement(false);
-        setLoad(false);
-      }
-    };
-
-    image1.addEventListener('load', gestionnaireChargementImage);
-    image2.addEventListener('load', gestionnaireChargementImage);
-
-    return () => {
-      image1.removeEventListener('load', gestionnaireChargementImage);
-      image2.removeEventListener('load', gestionnaireChargementImage);
-    };
-  }, []);
-
-
   return (
     <Box>
       <Container disableGutters sx={{ maxWidth: { xs: 'xs', sm: 'md', md: 'xs', lg: 'lg', xl: 'xs' } }}>
-        {
-          chargement ? (
-            <Backdrop
-              sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-              open={load}
-            >
-              <CircularProgress color="inherit" />
-            </Backdrop>
-          ) : (
-            <>
+        <>
 
-              <Stack className='animate__animated animate__slideInUp animate__fadeIn' mt={3} spacing={2} justifyContent='center' alignItems='center'>
-                <Typography
-                  variant='h6'
-                  component='h2'
+          <Stack className='animate__animated animate__slideInUp animate__fadeIn' mt={3} spacing={2} justifyContent='center' alignItems='center'>
+            <Typography
+              variant='h6'
+              component='h2'
+              sx={{
+                color: '#7f7f90',
+                fontWeight: 400,
+                letterSpacing: '1px',
+                fontSize: '13px',
+              }}
+            >A propos de nous</Typography>
+            <Typography variant='p' sx={{ textTransform: 'uppercase', fontWeight: '400', fontFamily: 'var( --font-primary)', fontSize: '40px', textAlign: 'center' }}>
+              En savoir plus
+              <Typography variant='span' sx={{ color: '#ce1212' }}> Sur nous</Typography>
+            </Typography>
+          </Stack>
+
+          <Grid container spacing={{ xs: 2, sm: 4, md: 8, lg: 8 }}>
+            <Grid item xs={12} sm={12} md={6}>
+              <Box
+                display='flex'
+                justifyContent='center'
+                alignItems='center'
+              >
+
+
+                <Box
+                  display='flex'
+                  alignItems="flex-end"
+                  justifyContent='center'
+                  mt={6}
                   sx={{
-                    color: '#7f7f90',
-                    fontWeight: 400,
-                    letterSpacing: '1px',
-                    fontSize: '13px',
+                    backgroundSize: 'cover',
+                    height: { xs: '400px', sm: '400px', md: '70vh', lg: '80vh' },
+                    width: { xs: '100%', sm: '90%', md: '100%' },
+                    backgroundPosition: 'center',
+                    position: 'relative',
                   }}
-                >A propos de nous</Typography>
-                <Typography variant='p' sx={{ textTransform: 'uppercase', fontWeight: '400', fontFamily: 'var( --font-primary)', fontSize: '40px', textAlign: 'center' }}>
-                  En savoir plus
-                  <Typography variant='span' sx={{ color: '#ce1212' }}> Sur nous</Typography>
-                </Typography>
-              </Stack>
+                >
 
-              <Grid container spacing={{ xs: 2, sm: 4, md: 8, lg: 8 }}>
-                <Grid item xs={12} sm={12} md={6}>
+                  <picture>
+                    <source
+                      type="image/webp"
+                      srcSet={`${heroIm}?width=100 100w, ${heroIm}?width=200 200w, ${heroIm}?width=400 400w, ${heroIm}?width=800 800w`}
+                    />
+                    <img src={heroIm} alt='d' width='100%' height='100%' />
+                  </picture>
+
+                  {/* <img
+                    src={heroIm}
+                    alt='d'
+                    width='100%'
+                    height='100%'
+                    style={{ objectFit: 'cover' }}
+                  /> */}
+
+                  <Paper sx={{ position: 'absolute', mb: '10px', padding: '40px', width: '80%', textAlign: 'center', fontWeight: 700, fontSize: '18px', backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
+                    <Typography variant='h4'>Reservation</Typography>
+                    <Typography variant='p' sx={{ color: '#ce1212' }}>+237 696746857</Typography>
+                  </Paper>
+                </Box>
+
+
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={12} md={6}>
+              <Box
+                display='flex'
+                flexDirection='column'
+                justifyContent='center'
+                alignItems='center'
+              >
+                <Box sx={{ width: { xs: '100%', sm: '90%', md: '100%' } }}>
+                  <Stack mt={4} sx={{ p: { xs: '0 12px', sm: '0 12px', md: '0' } }}>
+                    <Typography variant='p' sx={{ fontStyle: 'italic', color: '#212529', lineHeight: '25px' }}>
+                      Bienvenue à notre restaurant camerounais, une oasis de saveurs authentiques et de traditions culinaires. Notre établissement est une célébration de la richesse gastronomique du Cameroun, offrant une expérience unique à chaque visite. Découvrez nos délices culinaires, préparés avec amour et dévouement par nos chefs talentueux.<br /><br />
+                      Points forts de notre restaurant :
+                    </Typography>
+                    <List sx={{ color: '#212529' }}>
+                      <ListItem disableGutters sx={{ py: 1 }}>
+                        <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
+                        <ListItemText
+                          primary='Cuisine traditionnelle camerounaise'
+                        />
+                      </ListItem>
+                      <ListItem disableGutters sx={{ py: 1 }}>
+                        <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
+                        <ListItemText
+                          primary='Plats préparés avec des ingrédients frais et locaux'
+                        />
+                      </ListItem>
+                      <ListItem disableGutters sx={{ py: 1 }}>
+                        <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
+                        <ListItemText
+                          primary='Ambiance chaleureuse et accueillante'
+                        />
+                      </ListItem>
+                      <ListItem disableGutters sx={{ py: 1 }}>
+                        <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
+                        <ListItemText
+                          primary='Service exceptionnel de notre équipe dévouée'
+                        />
+                      </ListItem>
+                      <ListItem disableGutters sx={{ py: 1 }}>
+                        <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
+                        <ListItemText
+                          primary='Large sélection de vins et boissons'
+                        />
+                      </ListItem>
+                      <ListItem disableGutters sx={{ py: 1 }}>
+                        <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
+                        <ListItemText
+                          primary='Événements spéciaux et soirées à thème'
+                        />
+                      </ListItem>
+                    </List>
+
+                    <Typography variant='p' sx={{ color: '#212529', lineHeight: '25px' }}>
+                      Chez nous, chaque repas est une expérience culinaire inoubliable, et nous sommes impatients de vous accueillir pour partager la richesse de notre culture à travers nos plats délicieux.
+                    </Typography>
+                  </Stack>
                   <Box
+                    mt={4}
+                    mb={4}
                     display='flex'
+                    alignItems="center"
                     justifyContent='center'
-                    alignItems='center'
+                    sx={{
+                      background: `linear-gradient(rgba(15, 23, 43, 0.8), rgba(15, 23, 40, 0.8)), url(${about2})`,
+                      backgroundSize: 'cover',
+                      minHeight: '40vh',
+                      backgroundPosition: 'center',
+                      position: 'relative',
+
+                    }}
                   >
-
-
-                    <Box
-                      display='flex'
-                      alignItems="flex-end"
-                      justifyContent='center'
-                      mt={6}
-                      padding="25px 0"
-                      sx={{
-                        backgroundImage: `url(${heroIm})`,
-                        backgroundSize: 'cover',
-                        height: {xs: '400px', sm: '400px', md: '70vh', lg: '80vh'},
-                        width: { xs: '100%', sm: '90%', md: '100%' },
-                        backgroundPosition: 'center',
-                        position: 'relative',
-                      }}
-                    >
-
-                      <Paper sx={{ position: 'absolute', padding: '40px', width: '80%', textAlign: 'center', fontWeight: 700, fontSize: '18px', backgroundColor: 'rgba(255, 255, 255, 0.7)' }}>
-                        <Typography variant='h4'>Reservation</Typography>
-                        <Typography variant='p' sx={{ color: '#ce1212' }}>+237 696746857</Typography>
-                      </Paper>
-                    </Box>
-
+                    <IconButton>
+                      <PlayCircleTwoTone sx={{ fontSize: '80px', color: '#ce1212' }} />
+                    </IconButton>
 
                   </Box>
-                </Grid>
-                <Grid item xs={12} sm={12} md={6}>
-                  <Box
-                    display='flex'
-                    flexDirection='column'
-                    justifyContent='center'
-                    alignItems='center'
-                  >
-                    <Box sx={{ width: { xs: '100%', sm: '90%', md: '100%' } }}>
-                      <Stack mt={4} sx={{ p: { xs: '0 12px', sm: '0 12px', md: '0' } }}>
-                        <Typography variant='p' sx={{ fontStyle: 'italic', color: '#212529', lineHeight: '25px' }}>
-                          Bienvenue à notre restaurant camerounais, une oasis de saveurs authentiques et de traditions culinaires. Notre établissement est une célébration de la richesse gastronomique du Cameroun, offrant une expérience unique à chaque visite. Découvrez nos délices culinaires, préparés avec amour et dévouement par nos chefs talentueux.<br /><br />
-                          Points forts de notre restaurant :
-                        </Typography>
-                        <List sx={{ color: '#212529' }}>
-                          <ListItem disableGutters sx={{ py: 1 }}>
-                            <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
-                            <ListItemText
-                              primary='Cuisine traditionnelle camerounaise'
-                            />
-                          </ListItem>
-                          <ListItem disableGutters sx={{ py: 1 }}>
-                            <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
-                            <ListItemText
-                              primary='Plats préparés avec des ingrédients frais et locaux'
-                            />
-                          </ListItem>
-                          <ListItem disableGutters sx={{ py: 1 }}>
-                            <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
-                            <ListItemText
-                              primary='Ambiance chaleureuse et accueillante'
-                            />
-                          </ListItem>
-                          <ListItem disableGutters sx={{ py: 1 }}>
-                            <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
-                            <ListItemText
-                              primary='Service exceptionnel de notre équipe dévouée'
-                            />
-                          </ListItem>
-                          <ListItem disableGutters sx={{ py: 1 }}>
-                            <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
-                            <ListItemText
-                              primary='Large sélection de vins et boissons'
-                            />
-                          </ListItem>
-                          <ListItem disableGutters sx={{ py: 1 }}>
-                            <DoubleArrow sx={{ color: '#ce1212', mr: '2px' }} />
-                            <ListItemText
-                              primary='Événements spéciaux et soirées à thème'
-                            />
-                          </ListItem>
-                        </List>
+                </Box>
+              </Box>
+            </Grid>
+          </Grid>
 
-                        <Typography variant='p' sx={{ color: '#212529', lineHeight: '25px' }}>
-                          Chez nous, chaque repas est une expérience culinaire inoubliable, et nous sommes impatients de vous accueillir pour partager la richesse de notre culture à travers nos plats délicieux.
-                        </Typography>
-                      </Stack>
-                      <Box
-                        mt={4}
-                        mb={4}
-                        display='flex'
-                        alignItems="center"
-                        justifyContent='center'
-                        sx={{
-                          background: `linear-gradient(rgba(15, 23, 43, 0.8), rgba(15, 23, 40, 0.8)), url(${about2})`,
-                          backgroundSize: 'cover',
-                          minHeight: '40vh',
-                          backgroundPosition: 'center',
-                          position: 'relative',
-
-                        }}
-                      >
-                        <IconButton>
-                          <PlayCircleTwoTone sx={{ fontSize: '80px', color: '#ce1212' }} />
-                        </IconButton>
-
-                      </Box>
-                    </Box>
-                  </Box>
-                </Grid>
-              </Grid>
-
-            </>
-
-          )
-        }
-
+        </>
       </Container>
     </Box>
   );
--- src/components/Hero/Index.js
+++ src/components/Hero/Index.js
@@ -1,97 +1,71 @@
-import { Avatar, Backdrop, Box, CircularProgress, Container, IconButton, Stack, Typography } from '@mui/material'
+import { Avatar, Box, Container, IconButton, Stack, Typography } from '@mui/material'
 import ndole from '../../assets/images/ndole2.png'
 import { PlayCircleOutlined } from '@mui/icons-material'
-import { useEffect, useState } from 'react';
 const Hero = () => {
 
-  const [chargement, setChargement] = useState(true);
-  const [load, setLoad] = useState(true);
 
-  useEffect(() => {
-    const imageFond = new Image();
-    imageFond.src = ndole;
-
-    const gestionnaireChargementImage = () => {
-      setChargement(false);
-      setLoad(false)
-    };
-
-    imageFond.addEventListener('load', gestionnaireChargementImage);
-
-    return () => {
-      imageFond.removeEventListener('load', gestionnaireChargementImage);
-    };
-  }, []);
   return (
     <Box bgcolor='#eee'>
       <Container disableGutters sx={{ maxWidth: { xs: 'xs', sm: 'md', md: 'xs', lg: 'lg', xl: 'xs' } }}>
-        {chargement
+        <Box
+          alignItems='center'
+          justifyContent='space-between'
+          width='100%'
+          sx={{
+            display: 'flex',
+            flexDirection: { xs: 'column', sm: 'column', md: 'row-reverse' },
+            minHeight: '100vh',
+            alignItems: { xs: 'stretch', sm: 'stretch', md: 'center' },
+            backgroundSize: 'cover',
+            overflow: { xs: 'hidden', md: 'hidden' },
+            padding: { xs: '100px 0px 60px 0px', sm: '100px 0px 60px 0px', md: '90px 0px 60px 0px' }
+          }}
+          columnGap={20}
+        >
 
-          ? (
-            <Backdrop
-              sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-              open={load}
-            // onClick={handleClose}
-            >
-              <CircularProgress color="inherit" />
-            </Backdrop>
-          )
+          <Box px={2} flex={1} className='animate__animated animate__zoomIn' sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
 
-          : (
-            <Box
-              alignItems='center'
-              justifyContent='space-between'
-              width='100%'
+            <Avatar 
+              className='rotating-avatar'
+
+              loading='lazy' 
+              sx={{ borderRadius: 0, width: { xs: '100%', sm: '70%', md: '100%' }, height: '80%' }} 
+              src={ndole} 
+              alt='ndole' 
+              />
+
+          </Box>
+
+
+          <Box flex={1} className='animate__animated animate__slideInUp animate__fadeIn' display='flex' sx={{ flexDirection: { xs: 'column', sm: 'column', md: 'column' }, alignItems: { xs: 'center', sm: 'center', md: 'stretch' }, justifyContent: 'center' }}>
+            <Typography
+              variant='h6'
+              mt={4}
+              component='h2'
               sx={{
-                display: 'flex',
-                flexDirection: { xs: 'column', sm: 'column', md: 'row-reverse' },
-                minHeight: '100vh',
-                alignItems: { xs: 'stretch', sm: 'stretch', md: 'center' },
-                backgroundSize: 'cover',
-                overflow: { xs: 'hidden', md: 'hidden' },
-                padding: { xs: '100px 0px 60px 0px', sm: '100px 0px 60px 0px', md: '90px 0px 60px 0px' }
+                fontFamily: 'var(--font-primary)',
+                textAlign: { xs: 'center', sm: 'center', md: 'justify' },
+                color: '#37373f',
+                fontWeight: 700,
+                fontSize: { xs: '30px', sm: '50px' }
               }}
-              columnGap={20}
             >
-
-              <Box px={2} flex={1} className='animate__animated animate__zoomIn' sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', overflow: 'hidden' }}>
-
-                <Avatar className='rotating-avatar' sx={{ borderRadius: 0, width: { xs: '100%', sm: '70%', md: '100%' }, height: '80%' }} src={ndole} alt='ndole' />
-
-              </Box>
-
-
-              <Box flex={1} className='animate__animated animate__slideInUp animate__fadeIn' display='flex' sx={{ flexDirection: { xs: 'column', sm: 'column', md: 'column' }, alignItems: { xs: 'center', sm: 'center', md: 'stretch' }, justifyContent: 'center' }}>
-                <Typography
-                  variant='h6'
-                  mt={4}
-                  component='h2'
-                  sx={{
-                    fontFamily: 'var(--font-primary)',
-                    textAlign: { xs: 'center', sm: 'center', md: 'justify' },
-                    color: '#37373f',
-                    fontWeight: 700,
-                    fontSize: { xs: '30px', sm: '50px' }
-                  }}
-                >
-                  Profitez de votre santé <br />
-                  Nourriture délicieuse
-                </Typography>
-                <Typography mt={4} variant='p' sx={{ px: { xs: '10px', md: '0' }, color: '#4f4f5a', textAlign: { xs: 'center', sm: 'center', md: 'justify' }, fontWeight: 400, lineHeight: '22px' }}>
-                  Plongez dans une expérience culinaire unique, où chaque plat raconte une histoire, chaque saveur évoque la chaleur du Cameroun. Chez nous, la fraîcheur des ingrédients rencontre la tradition pour créer des repas mémorables.
-                </Typography>
-                <Stack direction='row' alignItems='center' mt={2}>
-                  <IconButton
-                    sx={{ color: '#ce1212' }}
-                  >
-                    <PlayCircleOutlined sx={{ fontSize: { xs: '35px', sm: '35px', md: '50px' } }} />
-                  </IconButton>
-                  <Typography variant='span'>Voir Video</Typography>
-                </Stack>
-              </Box>
-            </Box>
-          )}
-
+              Profitez de votre santé <br />
+              Nourriture délicieuse
+            </Typography>
+            <Typography mt={4} variant='p' sx={{ px: { xs: '10px', md: '0' }, color: '#4f4f5a', textAlign: { xs: 'center', sm: 'center', md: 'justify' }, fontWeight: 400, lineHeight: '22px' }}>
+              Plongez dans une expérience culinaire unique, où chaque plat raconte une histoire, chaque saveur évoque la chaleur du Cameroun. Chez nous, la fraîcheur des ingrédients rencontre la tradition pour créer des repas mémorables.
+            </Typography>
+            <Stack direction='row' alignItems='center' mt={2}>
+              <IconButton
+                sx={{ color: '#ce1212' }}
+              >
+                <PlayCircleOutlined sx={{ fontSize: { xs: '35px', sm: '35px', md: '50px' } }} />
+              </IconButton>
+              <Typography variant='span'>Voir Video</Typography>
+            </Stack>
+          </Box>
+        </Box>
       </Container>
     </Box>
 
